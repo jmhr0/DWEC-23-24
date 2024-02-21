ec896095594195a2e9d2f0a5ed8cd3499689f4c9
Juan Manuel <jherram0603@g.educaand.es>
2024-02-21 14:53:31 +0100

    dia 21/02/2024

--- T14/actividades/ActT14T2/index.js
+++ T14/actividades/ActT14T2/index.js
@@ -1,47 +1,92 @@
-import { MongoClient, ServerApiVersion } from 'mongodb';
-import express from 'express';
-import path from 'path';
-import bodyParser from 'body-parser';
+// Importamos los módulos necesarios
+import express from "express"; // Importamos Express.js para manejar las rutas y las solicitudes HTTP
+import path from "path"; // Importamos el módulo 'path' para manejar rutas de archivos y directorios
+import { MongoClient, ServerApiVersion } from "mongodb"; // Importamos el cliente MongoDB y la versión de la API del servidor
+import bodyParser from "body-parser"; // Importamos body-parser para analizar el cuerpo de las solicitudes HTTP
 
+// Inicializamos Express
 const app = express();
+
+// Resolvemos la ruta del directorio actual
 const __dirname = path.resolve();
 
-app.use(express.static(__dirname));
-app.use(bodyParser.json());
+// Configuramos aplicaciones middleware
+app.use(express.static(__dirname)); // Servimos archivos estáticos desde el directorio raíz del servidor
+app.use(bodyParser.json()); // Parseamos el cuerpo de las solicitudes HTTP con formato JSON
 
-const uri = "mongodb+srv://jherram:MFz2BbdNkUZqlwqd@cluster0.jb3gvow.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {serverApi: ServerApiVersion.v1 });
+// Creamos un enrutador utilizando express.Router()
+const router = express.Router();
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+// Definimos la cadena de conexión de MongoDB
+const uri =
+  "mongodb+srv://jherram:MFz2BbdNkUZqlwqd@cluster0.jb3gvow.mongodb.net/?retryWrites=true&w=majority";
+
+// Instanciamos un nuevo cliente MongoDB
+const client = new MongoClient(uri);
+
+// Definimos una ruta GET para la ruta raíz
+router.get("/", (req, res) => {
+  // Enviamos el archivo 'index.html' desde el directorio raíz del servidor
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
 
-app.get('/datos', async (req, res) => {
-    try {
-        await client.connect();
-        const coleccion = client.db("Cluster0").collection("T14ACT2");
-        const datos = await coleccion.find().toArray();
-        res.json(datos);
-    } catch (error) {
-        console.error(error);
-    } finally {
-        await client.close();
+// Ruta POST para manejar solicitudes
+app.post("/datos", async (req, res) => {
+  try {
+    // Verifica si el campo "nombre" no está vacío d
+    if (req.body.nombre !== "") {
+      await conectarDbWrite(req.body);
+      const datos = await conectarDbRead();
+      res.json(datos);
+    } else {
+      // Si el campo "nombre" está vacío, simplemente envía todos los datos existentes como respuesta JSON
+      const datos = await conectarDbRead();
+      res.json(datos);
     }
+  } catch (error) {
+    // Captura de errores
+    console.error(error);
+  } finally {
+    // Indica que ha seguido su curso correcto
+    console.log("Funciona correctamente");
+  }
 });
 
-app.post('/agregar', async (req, res) => {
-    try {
-        await client.connect();
-        const coleccion = client.db("Cluster0").collection("T14ACT2");
-        const nuevoDocumento = { nombre: req.body.nombre, apellidos: req.body.apellidos };
-        const resultado = await coleccion.insertOne(nuevoDocumento);
-        res.status(200).send('Usuario agregado con éxito');
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error al agregar el usuario');
-    } finally {
-        await client.close();
-    }
-});
+// Para leer datos de la base de datos
+async function conectarDbRead() {
+  try {
+    // Conecta a la base de datos y obtiene la colección de datos
+    await client.connect();
+    const coleccion = client.db("Cluster0").collection("T14ACT2");
+    // Lee todos los documentos de la colección y los devuelve como un array
+    const datos = await coleccion.find().toArray();
+    return datos;
+  } catch (error) {
+    // Captura y maneja cualquier error que pueda ocurrir
+    console.error(error);
+  } finally {
+    // Cierra la conexión con la base de datos
+    await client.close();
+  }
+}
 
-app.listen(3000, () => console.log('Escuchando en el puerto  3000'));
+// Para escribir datos en la base de datos
+async function conectarDbWrite(req) {
+  try {
+    // Conecta a la base de datos y obtiene la colección de datos
+    await client.connect();
+    const coleccion = client.db("Cluster0").collection("T14ACT2");
+    // Inserta un nuevo documento en la colección usando los datos recibidos
+    const nuevoDocumento = req;
+    const result = await coleccion.insertOne(nuevoDocumento);
+  } catch (error) {
+    // Captura y maneja cualquier error que pueda ocurrir
+    console.error(error);
+  } finally {
+    // Cierra la conexión con la base de datos
+    await client.close();
+  }
+}
+
+// Inicia el servidor Express.js y escucha en el puerto 3000
+app.listen(3000, () => console.log("Escuchando en el puerto 3000"));
